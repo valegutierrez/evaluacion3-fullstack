effa5a2a20eb21c1d616cac7226154a71ed26929
Valentina <vale.gutierrezc@gmail.com>
2018-11-29 20:01:25 -0300

    edit structure of index, new, form. create method 'show'

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -13,6 +13,10 @@ class TodosController < ApplicationController
     redirect_to todos_path
   end
 
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
   private
   def todo_params
     params.require(:todo).permit(:description, :completed)
