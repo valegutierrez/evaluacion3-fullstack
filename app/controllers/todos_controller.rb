ac5e779c1407234c4d495078f6740640b55bdd88
Valentina <vale.gutierrezc@gmail.com>
2018-11-29 19:40:47 -0300

    create method 'create' in 'todos_controller', modify routes and index structure

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -6,4 +6,15 @@ class TodosController < ApplicationController
   def end
     @todo = Todo.new
   end
+
+  def create
+    @todo = Todo.new(todo_params)
+    @todo.save
+    redirect_to todos_path
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:description, :completed)
+  end
 end
