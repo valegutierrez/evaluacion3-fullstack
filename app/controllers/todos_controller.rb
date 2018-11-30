433820c2bf1088eb16dd3a4be1a0859f1461d7ef
Valentina <vale.gutierrezc@gmail.com>
2018-11-29 21:47:35 -0300

    add delete method in index, fix render form, render navbar

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,10 +1,10 @@
 class TodosController < ApplicationController
-  before_action :find_todo, only: [:show, :edit, :update, :destroy]
+  before_action :find_todo, only: [:show, :edit, :update, :destroy, :complete]
   def index
     @todos = Todo.all
   end
 
-  def end
+  def new
     @todo = Todo.new
   end
 
@@ -21,6 +21,23 @@ class TodosController < ApplicationController
   end
 
   def update
+    @todo.update(todo_params)
+    redirect_to todos_path
+  end
+
+  def destroy
+    @todo.destroy
+    redirect_to todos_path
+  end
+
+  def complete
+    @todo.completed = true
+    @todo.save
+    redirect_to todos_path
+  end
+
+  def list
+    @todos = Todo.all
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root 'todos#index'
+  get '/todos/list'
   get '/todos', to: 'todos#index'
-  get 'todos/new'
   post '/todos', to: 'todos#create'
-  get 'todos/:id', to: 'todos#show', as: 'todo'
+  get 'todos/new'
   get 'todos/:id/edit', to: 'todos#edit', as: 'todo_edit'
+  get 'todos/:id', to: 'todos#show', as: 'todo'
   patch 'todos/:id', to: 'todos#update'
+  delete 'todos/:id', to: 'todos#destroy', as: 'todo_destroy'
+  get 'todos/:id/complete', to: 'todos#complete', as: 'todo_complete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
